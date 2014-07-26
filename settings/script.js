a2f2365d2366afd6735f1c99136f91f035317057
Amitay Dobo <amitayd@gmail.com>
2014-07-26 16:24:18 -0700

    Changed Multiple

--- settings/script.js
+++ settings/script.js
@@ -2,6 +2,19 @@ var codeStore = Tutorify.HttpCodeStore.create('../jsObjectPatterns/tutorify-js-o
     
 var settings = {
     fontSize: 14, //Font size to use for the text editor,
+    iframeEditor: false, // whether to load the editor in an iframe (to isolate dom),
+    iframeEditorSettings: {
+        acePath: '/lib/ace/src-min' // Configure the ace editor path for the iframe if needed
+    },
+    liveUpdate: true, // Should the code run after each change
+    resourceScripts: [ // Script src urls to include in the code when being run
+        {src: '/lib/jquery/jquery.min.js'}, 
+        {src: '/lib/hogan/hogan.js'}
+    ],
+    resourceCss: [ // Css to include when the code is being run
+        {src: 'result.css'}
+    ],
+
     
     
 };
