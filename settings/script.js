f6b3c9ebbc7256db15b256056c88a13f486c6565
Amitay Dobo <amitayd@gmail.com>
2014-07-26 15:40:40 -0700

    Changed Multiple

---
+++ settings/script.js
@@ -0,0 +1,8 @@
+var codeStore = Tutorify.HttpCodeStore.create('../jsObjectPatterns/tutorify-js-object-patterns/');
+    
+var settings = {
+    fontSize: 14, //Font size to use for the text editor,
+    
+    
+};
+Tutorify.init(settings, codeStore)
