d547e38590a8118bbe3a847afdc0d430bfa35759
Amitay Dobo <amitayd@gmail.com>
2014-07-26 15:22:55 -0700

    Changed Multiple

--- githubCodeStore/script.js
+++ githubCodeStore/script.js
@@ -1,4 +1,4 @@
-var accessToken = '641dfa2143facae4077274806a3a7b176cce553e', // try changing this to null for read-only
+var accessToken = null, // try changing this to null for read-only
     user = 'amitayd',
     repository = 'tutorify-playground',
     options = {};
